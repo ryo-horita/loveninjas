1be05ae69ac5077f834d9a81a098a0c8b0ea2c8a
Rei Takeda <rei.ski3190@gmail.com>
2023-12-11 21:04:12 +0900

    add file

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="default-container">
+    <header class="header">
+      <div class="logo">LOGO</div>
+
+      <ul class="menu">
+        <li class="item"><nuxt-link class="link" to="/">トップ</nuxt-link></li>
+        <li class="item"><nuxt-link class="link" to="/home">Home</nuxt-link></li>
+        <li class="item"><nuxt-link class="link" to="/about">About</nuxt-link></li>
+        <li class="item"><nuxt-link class="link" to="/videos">Videos</nuxt-link></li>
+      </ul>
+    </header>
+    <main class="main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.default-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  .header {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .menu {
+      display: flex;
+      list-style: none;
+      padding: 0;
+
+      .item {
+        margin-right: 20px;
+        .link {
+          color: #fff;
+          text-decoration: none;
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+
+      }
+    }
+  }
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    hello world<br/>
+    1. トップページを作る<br/>
+    2. ホーム画面をつくる（pages/home.vue）<br/>
+    3. 動画一覧画面（pages/videos.vue）<br/>
+    4. クイズ一覧画面（pages/questions.vue）<br/>
+    5. ...その他のページも必要に応じて作る<br/>
+    6. ヘッダーを作る（layouts/default.vue）<br/>
+    7. HOME画面の中身を作り込む (pages/home.vue)<br/>
+  </div>
+</template>
